680539f1223d607f627381c77d810dad899b2d8b
Dan Guest <dguest@cern.ch>
2019-08-22 05:51:25 +0200

    add a check for missing config files

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+Less-Shitty Analysis Base Image
+===============================
+
+The analysis base image is sort of shitty: not because the people who
+made it did a bad job, just because it's missing a lot of stuff that I
+like. This is an attempt to make it slightly less shitty.
+
 Running
 =======
 
--- run-docker.sh
+++ run-docker.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# we only get the ssh keys from your area if you haven't created them
+# and put them in `settings/ssh`
 get-ssh() {
     local TARG=${2-$1}
     mkdir -p settings/ssh
@@ -12,12 +14,24 @@ get-ssh id_rsa gitlab
 get-ssh id_rsa.pub gitlab
 get-ssh known_hosts
 
+# load in the ssh config files
 if ! [[ -f settings/ssh/config ]]; then
     cp ssh-default/config settings/ssh
 fi
 
+# load the user's git configuration
 mkdir -p settings/git
 if ! [[ -f settings/git/config ]]; then
+    if ! [[ -f ~/.gitconfig ]]; then
+        cat <<EOF >&2
+missing a .gitconfig, create one with git config, i.e. running
+
+git config --global user.name "John Doe"
+git config --global user.email johndoe@example.com
+
+EOF
+        exit 1
+    fi
     cp ~/.gitconfig settings/git/config
 fi
 
